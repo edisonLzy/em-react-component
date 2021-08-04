e351c4ec94a429ab78e4d9d06913364b1171140c
evanzyli <45064281+edisonLzy@users.noreply.github.com>
2021-08-04 22:19:31 +0800

    feat: 刷新404

--- .umirc.ts
+++ .umirc.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
     title:'测试',
     mode: 'site',
-    publicPath: `/edisonLzy.github.io/`,
-    exportStatic: {},
-    base: '/edisonLzy.github.io',
+    publicPath: `./`,
+    exportStatic: {}
 })
