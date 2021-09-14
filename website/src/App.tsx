4243256da6d78806b7dae45dd4ffc807e0b8b138
evanzyli <45064281+edisonLzy@users.noreply.github.com>
2021-09-14 21:34:22 +0800

    feat: add style

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Button from '@em-react-component/em-button';
+import '@em-react-component/em-button/lib/style/index.css';
 function App() {
   const [count, setCount] = useState(0);
 
